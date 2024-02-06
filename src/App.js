d99f658fc77c9a9e690ce93a1fc96c48579de91d
sheleshen <sheleshnewaanna@gmail.com>
2024-02-06 15:05:46 +0300

    Первичный end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     ПРИВЕТ!
-    </div>
-  );
+  return <div className="App">ПРИВЕТ!</div>;
 }
 
 export default App;
