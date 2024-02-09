df87c2ef998953fe509309f8309fcb9521e4698f
sheleshen <sheleshnewaanna@gmail.com>
2024-02-09 21:24:44 +0300

    Правки

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -3,25 +3,32 @@ import CounterButton from "components/CounterButton";
 import { useState } from "react";
 
 function Counter() {
+  const [count, setCount] = useState(1);
 
-    const [ count, setCount ] = useState(1)
-   
-    const minus = () => {
-        setCount(count + 1)
-    }
+  const minus = () => {
+    setCount(count + 1);
+  };
 
-    const plus = () => {
-        setCount(count - 1)
-    }
-    // сделать ограничение на отрицательное число!
+  const plus = () => {
+    setCount(count - 1);
+  };
+  // сделать ограничение на отрицательное число!
 
-    return (
-        <div className="flex gap-4 items-center">
-            <CounterButton onClick={minus} title='-' description='Уменьшить количество' />
-            <p className="text-slate-900 font-bold">{count}</p>
-            <CounterButton onClick={plus} title='+' description='Увеличить количество'/>
-        </div>
-    );
+  return (
+    <div className="flex gap-4 items-center">
+      <CounterButton
+        onClick={minus}
+        title="-"
+        description="Уменьшить количество"
+      />
+      <p className="text-slate-900 font-bold">{count}</p>
+      <CounterButton
+        onClick={plus}
+        title="+"
+        description="Увеличить количество"
+      />
+    </div>
+  );
 }
 
 export default Counter;
