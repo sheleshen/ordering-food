74f70abbf6431bcc081e5263a5eb5624251a2bd1
sheleshen <sheleshnewaanna@gmail.com>
2024-02-18 21:44:42 +0300

    Правки

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -40,51 +40,60 @@ function Cart() {
                 <Button
                   title={"Вернуться на главную"}
                   description={"Перейти на главную"}
+                  variant='default'
                 />
               </Link>
             </div>
           </div>
       ): 
 
-      <div className="px-6 md:px-4 lg:px-2 xl:px-0 py-10 md:py-12 lg:py-14 max-w-7xl m-auto ">
-        <p className="text-3xl md:text-4xl lg:text-5xl pb-10 md:pb-12 lg:pb-14 font-bold text-slate-400">
-          Товары в корзине
-        </p>
-
-        <div className="flex flex-col px-5 py-5 gap-4 bg-slate-100 rounded-3xl justify-between">
-          {cartItems.map((item) => (
-          
-
-          <div key={item.id} className="flex justify-between items-center">
-            <div className="flex gap-5">
-              <img alt="" src={item.image} className=" object-cover object-center lg:w-44 lg:h-44 md:w-40 md:h-40 w-28 rounded-3xl"></img>
-              <div className="flex flex-col gap-1">
-                <p className="text-xl md:text-2xl lg:text-3xl py-2 md:py-3 font-bold text-slate-800">
-                  {item.name} 
-                </p>
-                <p className="text-base md:text-lg font-bold text-amber-500">
-                  Цена: {item.price} руб.
-                </p>
-                <p className="text-sm md:text-base font-medium text-slate-600">
-                  Количество: <Counter />
-                </p>
-              
-              </div>
-            </div>
-            <div className="flex flex-col gap-2">
-              
-                <Button 
-                onClick={() => removeFromCart(item.id)}
-                title={'Удалить'}
-                variant='delete'
-                />
-            </div>
-          </div>
+      <div className="px-6 md:px-4 lg:px-2 xl:px-0 py-10 md:py-12 lg:py-14 max-w-7xl m-auto">
         
-        ))}
-</div>
-      </div>     
+          <p className="text-3xl md:text-4xl lg:text-5xl pb-10 md:pb-12 lg:pb-14 font-bold text-slate-400">
+            Товары в корзине
+          </p>
+        <div className="flex flex-col gap-10">
+            <div className="flex flex-col px-5 py-5 gap-4 bg-slate-100 rounded-3xl justify-between">
+              {cartItems.map((item) => (
+              
 
+              <div key={item.id} className="flex justify-between items-center">
+                <div className="flex gap-5">
+                  <img alt="" src={item.image} className=" object-cover object-center lg:w-44 lg:h-44 md:w-40 md:h-40 w-28 rounded-3xl"></img>
+                  <div className="flex flex-col gap-1">
+                    <p className="text-xl md:text-2xl lg:text-3xl py-2 md:py-3 font-bold text-slate-800">
+                      {item.name} 
+                    </p>
+                    <p className="text-base md:text-lg font-bold text-amber-500">
+                      Цена: {item.price} руб.
+                    </p>
+                    <p className="text-sm md:text-base font-medium text-slate-600">
+                      Количество: <Counter />
+                    </p>
+                  
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                  
+                    <Button 
+                    onClick={() => removeFromCart(item.id)}
+                    title={'Удалить'}
+                    variant='delete'
+                    />
+                </div>
+              </div>
+            
+            ))}
+            </div>
+
+            <Button
+                title={"Оформить заказ"}
+                description={"Оформить заказ"}
+                variant='default'
+              /> 
+          </div>
+        </div>     
+      
         
       };
       
